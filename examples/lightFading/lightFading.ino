353f1272317bcc3693e6ac6b3469e42b23f272d2
hodaig <hodaig@gmail.com>
2016-08-17 20:35:24 +0200

    add indication led to the example

--- examples/lightFading/lightFading.ino
+++ examples/lightFading/lightFading.ino
@@ -9,7 +9,7 @@
 
 #define STEPS 1
 
-int outPin = 4;   // the pin connected to the triac gate
+int outPin = 9;   // the pin connected to the triac gate
 uint8_t level = AC_DIMMER_VALUE_MIN;
 
 /* zero cros detector connected to pin 3 */
@@ -17,6 +17,7 @@ ACdimmer myDimmer(outPin, AC_DIMMER_PIN_3);
 
 void setup() {
   myDimmer.begin();
+  pinMode(10 , OUTPUT); // indication led
 }
 
 void loop() {
@@ -28,7 +29,8 @@ void loop() {
   }
 
   myDimmer.setValue(level);
-
+  
+  digitalWrite(10, level < 128 );
   delay(50);
 
 }
